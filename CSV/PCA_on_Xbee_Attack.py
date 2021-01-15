a95fb8c2867b283b9b866f07d8720e2440c1161a
Alex Robbins <xlvw@iup.edu>
2021-01-15 19:30:14 +0000

    Added h2o scripts for all csv files

---
+++ CSV/PCA_on_Xbee_Attack.py
@@ -0,0 +1,9 @@
+import h2o
+from h2o.estimators.pca import H2OPrincipalComponentAnalysisEstimator
+h2o.init()
+df = h2o.upload_file("Xbee_Attack.csv")
+print(df.head())
+pca_decomp = H2OPrincipalComponentAnalysisEstimator(k=2, transform="NONE", impute_missing=True)
+pca_decomp.train(training_frame=df)
+pred = pca_decomp.predict(df)
+print(pred.head())
